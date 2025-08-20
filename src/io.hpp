ba7723948fd756ba9eef29a8528cd37aa7234fc7
Kevin Exton <kevin.exton@pm.me>
2025-08-20 15:41:02 +1000

    factored out the customization point into a single customization point object.

--- src/io.hpp
+++ src/io.hpp
@@ -22,18 +22,8 @@
 #pragma once
 #ifndef IO_HPP
 #define IO_HPP
-#include "detail/accept.hpp"
-#include "detail/bind.hpp"
-#include "detail/connect.hpp"
-#include "detail/fcntl.hpp"
-#include "detail/getpeername.hpp"
-#include "detail/getsockname.hpp"
-#include "detail/getsockopt.hpp"
-#include "detail/listen.hpp"
-#include "detail/recvmsg.hpp"
-#include "detail/sendmsg.hpp"
-#include "detail/setsockopt.hpp"
-#include "detail/shutdown.hpp"
+#include "detail/customization.hpp"
+#include "socket/socket_ops.hpp"
 
 /**
  * @brief The `io` namespace contains all the functions and classes for the I/O
@@ -44,148 +34,33 @@
  * customized for different types of I/O objects.
  */
 namespace io {
-/**
- * @brief A customization point object for binding a socket to an address.
- *
- * This CPO can be used to bind a socket to a specific address. The actual
- * implementation is found by `tag_invoke`.
- *
- * @see tag_invoke
- *
- * Example:
- * @code
- * io::socket_handle sock{AF_INET, SOCK_STREAM, IPPROTO_TCP};
- * sockaddr_in addr{}
- * addr.sin_family = AF_INET;
- * addr.sin_addr.s_addr = INADDR_ANY;
- * addr.sin_port = 0;
- * // ...
- * ::io::bind(sock, &addr, sizeof(addr));
- * @endcode
- */
-inline constexpr detail::bind_fn bind{};
-
-/**
- * @brief A customization point object for setting a socket to listen for
- * incoming connections.
- *
- * This CPO can be used to set a socket to listen. The actual
- * implementation is found by `tag_invoke`.
- *
- * @see tag_invoke
- *
- * Example:
- * @code
- * io::socket_handle sock{AF_INET, SOCK_STREAM, IPPROTO_TCP};
- * // ...
- * int backlog = 16;
- * ::io::listen(sock, backlog);
- * @endcode
- */
-inline constexpr detail::listen_fn listen{};
-
-/**
- * @brief A customization point object that connects a socket to an address.
- *
- * This CPO finds a suitable implementation for connecting a socket via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::connect_fn connect{};
-
-/**
- * @brief A customization point object that accepts an incoming connection on a
- * listening socket.
- *
- * This CPO finds a suitable implementation for accepting a connection via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::accept_fn accept{};
-
-/**
- * @brief A customization point object that sends a message on a socket.
- *
- * This CPO finds a suitable implementation for sending a message via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::sendmsg_fn sendmsg{};
-
-/**
- * @brief A customization point object that receives a message from a socket.
- *
- * This CPO finds a suitable implementation for receiving a message via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::recvmsg_fn recvmsg{};
-
-/**
- * @brief A customization point object that gets a socket option.
- *
- * This CPO finds a suitable implementation for getting a socket option via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::getsockopt_fn getsockopt{};
-
-/**
- * @brief A customization point object that sets a socket option.
- *
- * This CPO finds a suitable implementation for setting a socket option via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::setsockopt_fn setsockopt{};
-
-/**
- * @brief A customization point object that gets the local address of a socket.
- *
- * This CPO finds a suitable implementation for getting the local address of a
- * socket via `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::getsockname_fn getsockname{};
-
-/**
- * @brief A customization point object that gets the peer address of a connected
- * socket.
- *
- * This CPO finds a suitable implementation for getting the peer address of a
- * socket via `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::getpeername_fn getpeername{};
-
-/**
- * @brief A customization point object that shuts down all or part of a
- * connection on a socket.
- *
- * This CPO finds a suitable implementation for shutting down a socket via
- * `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::shutdown_fn shutdown{};
-
-/**
- * @brief A customization point object that performs a file control operation on
- * a socket.
- *
- * This CPO finds a suitable implementation for performing a file control
- * operation on a socket via `tag_invoke`.
- *
- * @see tag_invoke
- */
-inline constexpr detail::fcntl_fn fcntl{};
+/// @brief A customization point object for binding a socket to an address.
+inline constexpr detail::cpo<socket::bind_t> bind{};
+/// @brief A customization point object for setting a socket to listen for
+inline constexpr detail::cpo<socket::listen_t> listen{};
+/// @brief A customization point object that connects a socket to an address.
+inline constexpr detail::cpo<socket::connect_t> connect{};
+/// @brief A customization point object that accepts an incoming connection on a
+/// listening socket.
+inline constexpr detail::cpo<socket::accept_t> accept{};
+/// @brief A customization point object that sends a message on a socket.
+inline constexpr detail::cpo<socket::sendmsg_t> sendmsg{};
+/// @brief A customization point object that receives a message from a socket.
+inline constexpr detail::cpo<socket::recvmsg_t> recvmsg{};
+/// @brief A customization point object that gets a socket option.
+inline constexpr detail::cpo<socket::getsockopt_t> getsockopt{};
+/// @brief A customization point object that sets a socket option.
+inline constexpr detail::cpo<socket::setsockopt_t> setsockopt{};
+/// @brief A customization point object that gets the local address of a socket.
+inline constexpr detail::cpo<socket::getsockname_t> getsockname{};
+/// @brief A customization point object that gets the peer address of a
+/// connected socket.
+inline constexpr detail::cpo<socket::getpeername_t> getpeername{};
+/// @brief A customization point object that shuts down all or part of a
+/// connection on a socket.
+inline constexpr detail::cpo<socket::shutdown_t> shutdown{};
+/// @brief A customization point object that performs a file control operation
+/// on a socket.
+inline constexpr detail::cpo<socket::fcntl_t> fcntl{};
 } // namespace io
 #endif // IO_HPP
