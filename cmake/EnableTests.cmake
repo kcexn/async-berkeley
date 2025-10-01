7e1c3ad2c179435528e1657f780b75e48427ccbb
Kevin Exton <kevin.exton@pm.me>
2025-10-01 12:11:39 +1000

    Update GoogleTest version to the latest stable release.

--- cmake/EnableTests.cmake
+++ cmake/EnableTests.cmake
@@ -8,7 +8,7 @@ message(STATUS "Configuring tests with GoogleTest")
 include(FetchContent)
 FetchContent_Declare(
     googletest
-    URL https://github.com/google/googletest/archive/52eb8108c5bdec04579160ae17225d66034bd723.zip
+    URL https://github.com/google/googletest/archive/refs/tags/v1.17.0.zip
 )
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
