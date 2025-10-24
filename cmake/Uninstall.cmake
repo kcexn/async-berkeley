4a182fb9d5f6134d7a32c96b3a75febad185d40e
Kevin Exton <kevin.exton@pm.me>
2025-10-24 18:14:19 +1100

    fix: Remove uninstall target.
    - The uninstall target pollutes downstream CMake configurations.
    Use the recommended `xargs rm < install_manifest.txt` approach instead.

--- cmake/Uninstall.cmake
+++
@@ -1,5 +0,0 @@
-# Add custom target for uninstall
-add_custom_target(uninstall
-    COMMAND "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake"
-    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
-)
